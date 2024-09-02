87e1478f7a1dd69db2efb39daf4118f00a2f7981
Mateus <mateus.olvr2002@gmail.com>
2024-09-02 14:56:01 -0300

    Finalização da resolução sem interface

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,36 @@
+package model.services;
+
+import java.time.Duration;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private BrazilTaxService taxService;
+	
+	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		Double minutes = (double) Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		Double hours = minutes / 60.0;
+		
+		Double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = pricePerHour * Math.ceil(hours);
+		}
+		else {
+			basicPayment = pricePerDay * Math.ceil(hours / 24.0);
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+}
